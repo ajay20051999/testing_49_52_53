2a0d55692c703fc7fad6de4ef88255e58127b7e4
ajay20051999 <abskjay@gmail.com>
2024-03-24 19:21:28 +0530

    adding three business logic

---
+++ comprehension.py
@@ -0,0 +1,25 @@
+tup = (10,20.3, 'python', 2+3j, 3.14, 10)
+print(tup)
+print(len(tup))
+print(type(tup))
+print(tup[0])
+print(tup[-1])
+print(tup[2:4])
+
+for i in tup:
+    print(i)
+
+    thislist = [10, 20, 30, "python"]
+
+    for i in thislist:
+        print(i)
+
+        a = 10
+        b = 20
+        c = a - b
+        print("subtraction of two nuber is :", c)
+
+        a = 10
+        b = 20
+        c = a - b
+        print("subtraction of two nuber is :", c)
